7c70a540e6ddec87b4e1a92e422c34b471781156
tangkong <tangkongrobert@gmail.com>
2021-06-30 13:41:42 -0700

    fixed info frame logic, added setup for config_attrs

--- profile_bluesky/startup/instrument/plans/cont_scan.py
+++ profile_bluesky/startup/instrument/plans/cont_scan.py
@@ -11,6 +11,7 @@ from bluesky.preprocessors import fly_during_decorator
 
 import ophyd
 from ophyd import Device
+from ophyd import Component as Cpt
 from ophyd.signal import EpicsSignal
 from ophyd.sim import det
 from ophyd.flyers import FlyerInterface
@@ -60,27 +61,42 @@ class FpgaFrameData(ctypes.Structure):
 # https://blueskyproject.io/tutorials/Flyer%20Basics.html
 # https://github.com/NSLS-II/sirepo-bluesky/blob/7173258e7570904295bfcd93d5bca3dcc304c15c/sirepo_bluesky/sirepo_flyer.py
 # https://github.com/thomascobb/bluefly/blob/0ee7ea837d5f74e3cdc69e2315e9beed28dd7766/bluefly/fly.py#L36
-class CXASFlyer(Device, FlyerInterface):
+
+class FPGABox(Device):
+    """
+    Device to be subclassed, contains the relevant PV's for the FPGA continuous scan box
+    """ 
+    # signals to interact with
+    data = Cpt(EpicsSignal, '.DATA')
+    trigger_signal = Cpt(EpicsSignal, '.TRIG')
+
+    # configuration signals
+    trigger_width = Cpt(EpicsSignal, '.TWID')
+    trigger_base_rate = Cpt(EpicsSignal, '.TBRT')
+
+class CXASFlyer(FPGABox, FlyerInterface):
     """CXASFlyer continuous x-ray spectroscopy flyer device
     Here lies jank, reader beware.
 
-    flyer = CXASFlyer(name='flyer')
+    flyer = CXASFlyer('BL22:SCAN:MASTER', name='flyer')
     """
-
-    def __init__(self, *args, **kwargs):
-        super().__init__('', parent=None, **kwargs)
+    def __init__(self, prefix, *, config_attrs=None, read_attrs=None, **kwargs):
         
         # Make status objects accessible by all methods
         self.kickoff_status = None
         self.complete_status = None
         self.t0 = time.time()
         self.last_update_time = None
-        self.trig = EpicsSignal('BL22:SCAN:MASTER.TRIG', name='trigger')
-        self.data = EpicsSignal('BL22:SCAN:MASTER.DATA', name='data')
+        #self.trig = EpicsSignal('BL22:SCAN:MASTER.TRIG', name='trigger')
+        #self.data = EpicsSignal('BL22:SCAN:MASTER.DATA', name='data')
 
         # TO-DO: configuration PV's to keep track of, for derived data
-
-        print('initialized')
+        if config_attrs is None:
+            config_attrs = ['trigger_width', 'trigger_base_rate']
+        if read_attrs is None: 
+            read_attrs = []
+        super().__init__(prefix, **kwargs) # config attrs kinda broken at the moment?  Disabling works
+                            #read_attrs=read_attrs, **kwargs)
 
     def kickoff(self):
         logger.info("kickoff()")
@@ -121,6 +137,8 @@ class CXASFlyer(Device, FlyerInterface):
         self.encoder     = np.zeros(self.num_frames * self.num_encoder, np.uint32)
         self.gate        = np.zeros(self.num_frames, np.uint32)
         self.time        = np.zeros(self.num_frames, np.uint32)
+        print('k', self.time)
+        print('k', self.num_frames)
 
         # initialize data format for describe_collect(), collect()
         self.buffer_dict = [] # will store event dictionaries
@@ -142,28 +160,64 @@ class CXASFlyer(Device, FlyerInterface):
             return
 
         # Kickoff activity here. Set up activity that runs when PV's update
-        self.trig.put(2) # enable continuous scanning
-        self.trig.subscribe(self._trigger_changed) 
+        self.trigger_signal.subscribe(self._trigger_changed) 
         self.data.subscribe(self._data_update)
 
+        self.trigger_signal.put(2) # enable continuous scanning
         # once started, notify by updating status object
         self.kickoff_status._finished(success=True)
 
         # Wait for completion, leave this to the _trigger_changed method
 
     def _trigger_changed(self, value=None, old_value=None, **kwargs):
-        "This is called when the 'trigger' signal changes."
+        "This is called when the 'trigger_signal' changes."
         if self.complete_status is None:
             return
-        if old_value > value: #(old_value == 1) and (value == 0):
+        if old_value > value: #(old_value == 1 or 2) and (value == 0):
             # Negative-going edge means an acquisition just finished.
             self.complete_status._finished()
             self.complete_status = None # wrap up for next run
-
+    
     def _data_update(self, value=None, timestamp=None, **kwargs):
         logging.info('_data_update()')
+        print('_data_update()')
         pv_data = self.data.get()
+        # initialize parameters
+        self.num_frames         = np.zeros(1, np.uint32)
+        self.num_adc            = np.zeros(1, np.uint32)
+        self.num_counter        = np.zeros(1, np.uint32)
+        self.num_encoder        = np.zeros(1, np.uint32)
+        self.num_motor          = np.zeros(1, np.uint32)
+        self.last_error_code    = np.zeros(1, np.uint32)
+
+        pv_data = self.data.get() # array of ints
+
+        # get info from the FPGA box and record
+        fpgaFrameInfo = FpgaFrameInfo(0,
+                                np.ctypeslib.as_ctypes(pv_data),
+                                
+                                np.ctypeslib.as_ctypes(self.num_frames),
+                                
+                                np.ctypeslib.as_ctypes(self.num_adc),
+                                np.ctypeslib.as_ctypes(self.num_counter),
+                                np.ctypeslib.as_ctypes(self.num_encoder),
+                                np.ctypeslib.as_ctypes(self.num_motor),
+                                # 1 for success
+                                np.ctypeslib.as_ctypes(self.last_error_code))
+
+        fpga_eval_lib.GetFpgaFrameInfo.restype = None
+        fpga_eval_lib.GetFpgaFrameInfo(ctypes.byref(fpgaFrameInfo)) 
+
+        # initialize data 
+        self.adc         = np.zeros(self.num_frames * self.num_adc, np.uint32)
+        self.counter     = np.zeros(self.num_frames * self.num_counter, np.uint32)
+        self.motor       = np.zeros(self.num_frames * self.num_motor, np.uint32)
+        self.encoder     = np.zeros(self.num_frames * self.num_encoder, np.uint32)
+        self.gate        = np.zeros(self.num_frames, np.uint32)
+        self.time        = np.zeros(self.num_frames, np.uint32)
+
         # parse new frame data
+        #print('--init fpgaFrameData')
         fpgaFrameData = FpgaFrameData (	0,
 								np.ctypeslib.as_ctypes(pv_data),
 								
@@ -187,16 +241,19 @@ class CXASFlyer(Device, FlyerInterface):
         
 
         # C function call
+        #print('--calling EvalFpgaData')
         fpga_eval_lib.EvalFpgaData.restype = None
         fpga_eval_lib.EvalFpgaData(ctypes.byref(fpgaFrameData))
-        
+       
         # if we don't have a new frame
+        #print('--checking timestamps')
         if self.last_update_time == self.time[0]:
             return # we don't have a new frame
         else: 
             self.last_update_time = self.time[0] # remember last update time
 
         # for each frame in self.frame_num, append to buffer
+        #print('--setup dictionary')
         for frame in range(self.num_frames.item()):
             curr_frame = {}
             curr_frame.update({'time': self.time[frame]})
@@ -209,8 +266,15 @@ class CXASFlyer(Device, FlyerInterface):
                                   for i in range(self.num_encoder.item())})
             curr_frame.update({f'counter_{i}': self.counter[frame+i] 
                                 for i in range(self.num_counter.item())})
-           
-            self.buffer_dict.append(curr_frame)
+
+            t = time.time() 
+            time_frame ={}
+            for key in curr_frame.keys():
+                time_frame.update({key:t})
+            #print('  --append to buffer')
+            self.buffer_dict.append({'time':t,
+                                    'data':curr_frame,
+                                    'timestamps':time_frame})
 
     def complete(self):
         logger.info('complete()')
@@ -224,14 +288,33 @@ class CXASFlyer(Device, FlyerInterface):
         # currently only single stream
         d = dict(
             source = self.data.pvname,
-            dtype = 'array', 
+            dtype = 'number', 
             shape = []
         )
-
-        self.desc = {self.name: { 
-                        'data' : d
+        
+        
+        self.desc = {self.name:   # hard code for now to check
+                        dict(
+                            time=d, 
+                            gate=d,
+                            motor_0=d,
+                            motor_1=d,
+                            motor_2=d,
+                            motor_3=d,
+                            adc_0=d,
+                            adc_1=d,
+                            adc_2=d,
+                            adc_3=d,
+                            adc_4=d,
+                            adc_5=d,
+                            adc_6=d,
+                            adc_7=d,
+                            encoder_0=d,
+                            encoder_1=d,
+                            encoder_2=d,
+                            encoder_3=d,
+                            )
                     }
-        }
 
     def describe_collect(self):
         """
@@ -282,11 +365,11 @@ def fly_plan(flyer, *, md=None):
     yield from bps.kickoff(flyer, wait=True)
     complete_status = yield from bps.complete(flyer, wait=False)
     while not complete_status.done:
-        yield from bps.sleep(10) # rate limit @ 40Hz
+        yield from bps.sleep(0.1) # rate limit @ 40Hz
         yield from bps.collect(flyer)
 
     yield from bps.close_run()
     return uid
 
 
-flyer = CXASFlyer(name='flyer')
+flyer = CXASFlyer('BL22:SCAN:MASTER', name='flyer') #, configuration_attrs=['trigger_width'])
